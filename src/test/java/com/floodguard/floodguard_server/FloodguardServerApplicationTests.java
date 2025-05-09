282fad28b0991916874919067688c16c444d6cb8
Bruno De Almeida Otero <pt3025535@ifsproot.ptb>
2025-05-09 19:30:40 -0300

    initial commit

---
+++ src/test/java/com/floodguard/floodguard_server/FloodguardServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.floodguard.floodguard_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodguardServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
